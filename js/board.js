fbdaafdddfa64ea08791ab56cef4de4d5a6fe17f
singamdeepak5@gmail.com <you@example.com>
2019-11-04 17:44:05 +0530

    modified the js file

--- js/board.js
+++ js/board.js
@@ -2,11 +2,13 @@ let a= ['1A','2A','3A','4A','5A','6A','7A','8A','9A','10A','JA','QA','KA','AA'
     ,'1C','2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AC',
     '1H','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AH',
     '1S','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS','AS'];
-let pile1;
-let ph1,ph2,ph3;
-let ph;
+let pile1=[];
+let ph1=[],ph2=[],ph3=[];
+let ph=[];
 let active=[ph1,ph2,ph3];
-let turid=0;
+let turnid=0;
+let x,check,check2,check3;
+let z=[];
 function turn()
 { 
 
@@ -95,25 +97,67 @@ function draw()
 
 function start()
 {  
+  var rannum=[];
+  temp2=1;
+  for(let k = 1 ; k < 53; k++)
+  {
+     rannum[k]= temp2;
+     temp2++;
+  }
+  let b=0;
+  let u=0;
+  let temp=0;
+  for(i=0 ;i <8 ;i++)
+  {
+            var x = rannum[Math.floor(Math.random()*rannum.length)];
+            if(u!=1)
+               { for(i =0 ;i <u ; i++)
+                   {  if(z[i]==x)
+                       { 
+                        temp=1;break;
+                        }
+                      else  continue;
+                      }
+                   if(temp!=0)
+                        x=x-4;
+                }
+                
+                ph[u]= x;
+                 u++;
+               console.log(x);
 
-  for(var i=1;i<8;i++)
-    {
-      
-    ph[i]=String.random(a);
-  
-       for( let i1 = 0; i1 < a.length; i1++)
-       { 
-           if ( a[i1] == ph[i] ) 
-           {
-              a.splice(i1, 1);      //i1-location,1 element
-              i1--;
-              
-           }
-           break;
-        }
-    }
+  for(let k = 1 ; k < 53; k++)
+  {
+
+    if ( rannum[k] == x  ) 
+           {  ph[b]=rannum[k];
+              b++;
+              rannum.splice(k, 1);      //location of element
+              k--;
+              break;
+            }
+             if(b==6)
+               break;
+   }
+            
+  }
+  for(i = 0 ; i <8 ; i++)
+   {
+      for(j= 1; j< 53;j++)
+      {
+        if(ph[i]==j)
+        {
+        ph1[i]=a[j];
+        a.splice(j,1);
+        break;                        //finding and removing elements
+      }
+     } 
+   }
+
+console.log(ph1);
+
     
-    return ph;
+    return ph1;
   }
 function game()
 {
@@ -143,3 +187,4 @@ function game()
       }
 
 game();
+
